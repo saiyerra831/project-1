657a6196be69a5ac584b33cd336a64084f44e6a1
saiyerra831 <130850844+saiyerra831@users.noreply.github.com>
2023-06-21 01:12:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "tf-ec1" {
  ami = "ami-090e0fc566929d98b"
  instance_type = "t2.micro"
- key_name = "tests"
  vpc_security_group_ids = ["${aws_security_group.tf-sg.id}"]
  subnet_id = "${aws_subnet.tf-pub-sn-1.id}"
  associate_public_ip_address = true
