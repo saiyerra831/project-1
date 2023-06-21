ed7356623a451856403e9abc34c336e06070399a
saiyerra831 <130850844+saiyerra831@users.noreply.github.com>
2023-06-21 09:50:17 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "tf-alb" {
-  name               = "External-LB"
+  name               = "external-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.tf-sg.id]
